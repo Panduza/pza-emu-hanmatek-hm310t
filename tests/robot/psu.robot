7a7fdaa0e023b7545cc8339e262f5f647f353bc9
quentin <quentin.chopinet@gmail.com>
2023-06-08 14:47:53 +0200

    add write tests

--- tests/robot/psu.robot
+++ tests/robot/psu.robot
@@ -4,8 +4,26 @@ Library           ../rsc/Client.py
 *** Test Cases ***
 Read voltage
     Read voltage goal
+
+Read Amps
+    Read amps goal
+
 Write voltage
     Write voltage goal    20
+    Voltage goal should be    20
     Voltage real should be    20
     Write voltage goal    40
+    Voltage goal should be    40
     Voltage real should be    40
+
+Write Amps max
+    Write amps goal    200
+    Amps goal should be    200
+    Write amps goal    500
+    Amps goal should be    500
+
+Turn ON OFF power supply
+    Turn on power supply
+    Power Supply Should Be    on
+    Turn off power supply
+    Power Supply Should Be    off
--- tests/rsc/Client.py
+++ tests/rsc/Client.py
@@ -16,6 +16,9 @@ class Client:
         print("voici comment imprimer dans le log.html")
         return "le texte etait: %s" % (texte)
     
+
+    # --- GETS ---
+
     def read_voltage_goal(self):
         voltageGoal = self.master.read_holding_registers(address=0x30, count=1, slave=0x01)
         return voltageGoal.registers[0]
@@ -36,6 +39,9 @@ class Client:
         enable = self.master.read_holding_registers(address=0x01, count=1, slave=0x01)
         return enable.registers[0]
         
+
+    # --- SET ---
+
     def write_voltage_goal(self, voltage):
         sending = self.master.write_registers(0x30, int(voltage), 0x01)
 
@@ -45,10 +51,44 @@ class Client:
     def write_enable(self, state):
         sending = self.master.write_registers(0x1, int(state) , 0x01)
 
+    def turn_on_power_supply(self):
+        self.write_enable(1)
+
+    def turn_off_power_supply(self):
+        self.write_enable(0)
+
+
+    # --- CHECK ---
+
     def voltage_real_should_be(self, voltageExpected):
         voltageReal = self.read_voltage_real()
         if voltageReal != int(voltageExpected):
             raise AssertionError('%s != %s' % (voltageReal, int(voltageExpected)))
+    
+    def voltage_goal_should_be(self, voltageExpected):
+        voltageGoal = self.read_voltage_goal()
+        if voltageGoal != int(voltageExpected):
+            raise AssertionError('%s != %s' % (voltageGoal, int(voltageExpected)))
+        
+    def amps_real_should_be(self, apmsExpected):
+        ampsReal = self.read_amps_real()
+        if ampsReal != int(apmsExpected):
+            raise AssertionError('%s != %s' % (ampsReal, int(apmsExpected)))
+
+    def amps_goal_should_be(self, apmsExpected):
+        ampsGoal = self.read_amps_goal()
+        if ampsGoal != int(apmsExpected):
+            raise AssertionError('%s != %s' % (ampsGoal, int(apmsExpected)))
+
+    def enable_should_be(self, enableExpected):
+        enableReal = self.read_enable()
+        if enableReal != int(enableExpected):
+            raise AssertionError('%s != %s' % (enableReal, int(enableExpected)))
+        
+    def power_supply_should_be(self, enableExpected):
+        state = {"on": "1", "off": "0"}
+        self.enable_should_be(state[enableExpected])
+
 
 if __name__ == '__main__':
     interface = Client()
@@ -56,5 +96,7 @@ if __name__ == '__main__':
     print("***"+str(voltageReal)+"***")
     print(voltageReal)
     print(type(voltageReal))
+
+    print(interface.read_amps_real())
 
     

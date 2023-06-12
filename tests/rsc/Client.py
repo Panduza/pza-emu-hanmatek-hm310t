bb668050cc0e8a233a2aa6d83bdd8d17bb62f724
quentin <quentin.chopinet@gmail.com>
2023-06-12 15:21:48 +0200

    reorganize test order, add consumption and resistor tests

--- tests/rsc/Client.py
+++ tests/rsc/Client.py
@@ -11,11 +11,11 @@ class Client:
         connexion = self.master.connect()
         print(self.master)
         print("\033[92m-------------------client connected----------------------\033[0m")
-        
+
     def mon_keyword_perso(self, texte):
         print("voici comment imprimer dans le log.html")
         return "le texte etait: %s" % (texte)
-    
+
 
     # --- GETS ---
 
@@ -24,7 +24,7 @@ class Client:
         content = content.registers[0]
         print("PSU model = " + str(content))
         return content
-    
+
     def read_psu_class(self):
         content = self.master.read_holding_registers(address=0x04, count=1, slave=0x01)
         content = content.registers[0]
@@ -36,31 +36,31 @@ class Client:
         voltageGoal = voltageGoal.registers[0]
         print("Voltage goal = " + str(voltageGoal))
         return voltageGoal
-    
+
     def read_voltage_real(self):
         voltageReal = self.master.read_holding_registers(address=0x10, count=1, slave=0x01)
         voltageReal = voltageReal.registers[0]
         print("Voltage real = " + str(voltageReal))
         return voltageReal
-        
+
     def read_amps_goal(self):
         ampsGoal = self.master.read_holding_registers(address=0x31, count=1, slave=0x01)
         ampsGoal = ampsGoal.registers[0]
         print("Amps goal = " + str(ampsGoal))
         return ampsGoal
-        
+
     def read_amps_real(self):
         ampsReal = self.master.read_holding_registers(address=0x11, count=1, slave=0x01)
         ampsReal = ampsReal.registers[0]
         print("Amps real = " + str(ampsReal))
         return ampsReal
-        
+
     def read_enable(self):
         enable = self.master.read_holding_registers(address=0x01, count=1, slave=0x01)
         enable = enable.registers[0]
         print("Enable status = " + str(enable))
         return enable
-    
+
     def read_power_consumption(self):
         powerH = self.master.read_holding_registers(address=0x12, count=1, slave=0x01)
         powerL = self.master.read_holding_registers(address=0x13, count=1, slave=0x01)
@@ -71,7 +71,12 @@ class Client:
         print("Power consumption H = " + str(powerH))
         print("Power consumption = " + str(power))
         return power
-        
+
+    def read_resistor_load(self):
+        enable = self.master.read_holding_registers(address=0x024, count=1, slave=0x01)
+        enable = enable.registers[0]
+        print("Resistor load = " + str(enable))
+        return enable
 
     # --- SET ---
 
@@ -98,6 +103,9 @@ class Client:
     def turn_off_power_supply(self):
         self.write_enable(0)
 
+    def write_resistor_load(self, value):
+        sending = self.master.write_registers(0x24, int(value) , 0x01)
+
 
     # --- CHECK ---
 
@@ -114,31 +122,41 @@ class Client:
     def voltage_real_should_be(self, voltageExpected):
         voltageReal = self.read_voltage_real()
         if voltageReal != int(voltageExpected):
-            raise AssertionError('%s != %s' % (voltageReal, int(voltageExpected)))
-    
+            raise AssertionError('Voltage real received is %s, the expected value is %s.' % (voltageReal, int(voltageExpected)))
+
     def voltage_goal_should_be(self, voltageExpected):
         voltageGoal = self.read_voltage_goal()
         if voltageGoal != int(voltageExpected):
-            raise AssertionError('%s != %s' % (voltageGoal, int(voltageExpected)))
-        
+            raise AssertionError('Voltage goal received is %s, the expected value is %s' % (voltageGoal, int(voltageExpected)))
+
     def amps_real_should_be(self, apmsExpected):
         ampsReal = self.read_amps_real()
         if ampsReal != int(apmsExpected):
-            raise AssertionError('%s != %s' % (ampsReal, int(apmsExpected)))
+            raise AssertionError('The Amps real received is %s, the expected value is %s' % (ampsReal, int(apmsExpected)))
 
     def amps_goal_should_be(self, apmsExpected):
         ampsGoal = self.read_amps_goal()
         if ampsGoal != int(apmsExpected):
-            raise AssertionError('%s != %s' % (ampsGoal, int(apmsExpected)))
+            raise AssertionError('The Amps goal received is %s, the expected value is %s' % (ampsGoal, int(apmsExpected)))
 
     def enable_should_be(self, enableExpected):
         enableReal = self.read_enable()
         if enableReal != int(enableExpected):
-            raise AssertionError('%s != %s' % (enableReal, int(enableExpected)))
-        
+            raise AssertionError('The Enale status received is %s, the expected value is %s' % (enableReal, int(enableExpected)))
+
     def power_supply_should_be(self, enableExpected):
         state = {"on": "1", "off": "0"}
         self.enable_should_be(state[enableExpected])
+    
+    def power_consumption_should_be(self, powerExpected):
+        power = self.read_power_consumption()
+        if power != int(powerExpected):
+            raise AssertionError('The power consumption received is %s, the expected value is %s' % (power, int(powerExpected)))
+
+    def resistor_load_should_be(self, resistorExpected):
+        resistor = self.read_resistor_load()
+        if resistor != int(resistorExpected):
+            raise AssertionError('The power consumption received is %s, the expected value is %s' % (resistor, int(resistorExpected)))
 
 
 if __name__ == '__main__':
@@ -151,4 +169,4 @@ if __name__ == '__main__':
     print(interface.read_amps_real())
     print(interface.read_amps_goal())
 
-    
+
